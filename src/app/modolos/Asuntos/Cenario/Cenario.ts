48cc984ef0138e2726ad39d47e1f4d51f69ed77f
Marcos <marcos@MarcosDEB>
2025-10-28 18:48:50 -0300

    v0.6.5

--- src/app/modolos/Asuntos/Cenario/Cenario.ts
+++ src/app/modolos/Asuntos/Cenario/Cenario.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { NgOptimizedImage } from "@angular/common";
+import { Bt } from "../../coponetes/botão/Bt";
 
 @Component({
     selector: "Cenario",
     standalone: true,
     templateUrl: "./s1.html",
     styleUrls: ["./s1.css","../../../../assets/css/global.css"],
-    imports: [NgOptimizedImage]
+    imports: [NgOptimizedImage, Bt]
 }) export class Cenario {
     readonly IM = 'assets/images/mapa.png';
     readonly at = 'Mapa do Brasil com visualização de dados de cibersegurança sobreposto.';
---
+++ src/app/modolos/coponetes/botão/Bt.ts
@@ -0,0 +1,11 @@
+import { Component, Input} from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+    selector: "Bt",
+    templateUrl: "./Bt.html",
+    styleUrl: './Bt.css',
+    imports: [RouterLink]
+}) export class Bt {
+    @Input() rota: string = "rota";
+}
